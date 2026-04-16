886ba2088b7a07badbfb61199a7b06897cef092a
veronika <sustv153@gmail.com>
2026-04-16 16:14:43 +0300

    note

--- types/note.ts
+++ types/note.ts
@@ -2,5 +2,7 @@ export interface Note {
   id: string;
   title: string;
   content: string;
-  tag: string;
+  tag?: string;
+  createdAt: string;
 }
+export type NewNote = Omit<Note, 'id' | 'createdAt'>;
