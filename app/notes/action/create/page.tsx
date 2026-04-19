56301f48bb16c088f112fb709e2bce8916157020
veronika <sustv153@gmail.com>
2026-04-19 20:22:55 +0300

    create page action

--- app/notes/action/create/page.tsx
+++ app/notes/action/create/page.tsx
@@ -4,13 +4,27 @@ import NoteForm from "@/components/NoteForm/NoteForm";
 export const metadata: Metadata = {
   title: "Create Note",
   description: "Page for creating a new note",
+  openGraph: {
+    title: "Create Note",
+    description: "Page for creating a new note",
+    url: "https://08-zustand-eight-eta.vercel.app/notes/action/create", 
+    images: [
+      {
+        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Create Note Preview",
+      },
+    ],
+    type: "website",
+  },
 };
 
 export default function CreateNotePage() {
   return (
-    <div style={{ padding: 24 }}>
+    <main style={{ padding: 24 }}>
       <h1>Create Note</h1>
       <NoteForm />
-    </div>
+    </main>
   );
 }
